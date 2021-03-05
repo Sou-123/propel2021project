07733a1b3f9d14d63c5af189005bb9d8eb8257e6
soumya <soumyamanuel>
2021-03-05 17:29:19 +0530

    Second commit

--- DemoGitApp/src/com/faith/app/DemoApp.java
+++ DemoGitApp/src/com/faith/app/DemoApp.java
@@ -5,7 +5,9 @@ public class DemoApp {
 	public static void main(String[] args) {
 		
 		//Demonstration on GitHub-Push/Fetch
-		System.out.println("Welcome to the world of Git");
+		System.out.println("Welcome to the world of Git...");
+		
+		System.out.println("Hello");
 
 	}
 
